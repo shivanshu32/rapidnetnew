e62d5bf11256802c3d0d9e5b2a549bf607f6307d
AbscodInformatics <shivanshupathak@Shivanshus-MacBook-Air.local>
2023-10-21 10:16:17 +0530

    testjadsf

--- src/components/services/ServiceCardSection.tsx
+++ src/components/services/ServiceCardSection.tsx
@@ -16,7 +16,7 @@ const ServiceCardSection = () => {
                                 <div className="services-content">
                                     <h3 className="title"><Link href={`/service-details/${item.id}`}>{item.title}</Link></h3>
                                     <p>{item.teaser}</p>
-                                    <Link href={`/service-details/${item.id}`} className="btn btn-link hover-underline">{item.btn}</Link>
+                                    <Link href={`/service-details/${item.id}`} className="btn btn-link hover-underline"></Link>
                                 </div>
                             </div>
                         </div>
